0fe7ea9831fda4698c57824cdc64a62942b5705a
Nicole Kaff <nkaff@wireless-10-104-227-211.umd.edu>
2022-05-12 14:38:35 -0400

    python script bug fix

--- tools/excell_to_json.py
+++ tools/excell_to_json.py
@@ -7,7 +7,7 @@ df = pd.read_excel(sys.argv[1])
 df= df.rename(columns=str.lower)
 
 for i in df.index:
-    name = df.iloc[i]['Image']
+    name = df.iloc[i]['image']
     prefix = name.split(".")[0]
     df.loc[i].to_json("{}.json".format(prefix))
 
