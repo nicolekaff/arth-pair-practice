efc959dc6ea071b130361be1ad2258ce1b23a2da
Nicole Kaff <nkaff@wireless-10-104-227-211.umd.edu>
2022-05-12 14:30:50 -0400

    python df to lowercase

--- tools/excell_to_json.py
+++ tools/excell_to_json.py
@@ -4,6 +4,7 @@ import sys
 # Run with: python3 excell_to_json.py EXCELL_FILE_NAME
 
 df = pd.read_excel(sys.argv[1])
+df= df.rename(columns=str.lower)
 
 for i in df.index:
     name = df.iloc[i]['Image']
